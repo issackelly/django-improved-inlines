1ce5bba4b53b0d64767d9bdd81cb09255fd098f8
Issac Kelly <issackelly@alpha.insight.rr.com>
2010-05-29 11:09:52 -0400

    Figuring out the cheeseshop

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
 import os
 from distutils.core import setup
  
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
- 
-README = read('README.markdown')
+#f = open(os.path.join(os.path.dirname(__file__), 'README.markdown'))
+#readme = f.read()
+#f.close()
  
 setup(
     name = "django-improved-inlines",
-    version = "0.1.0a",
+    version = "0.1.3a",
     url = 'http://github.com/issackelly/django-improved-inlines',
     license = 'BSD',
     description = "Inline object rendering for django, based on django-basic-apps + filters + templates",
-    long_description=README,
     author = 'Issac Kelly',
     author_email = 'issac@kellycreativetech.com',
     packages = [
         'inlines',
         'inlines.templatetags',
     ],
+    #long_description=readme,
+    package_data={'inlines': ['*.markdown']},
     requires = [
         'BeautifulSoup',
     ],
